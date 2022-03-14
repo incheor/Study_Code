32ee6543e10f9133b2fa1da10637af1e6ec076ee
incheor <xornjscjf08@gmail.com>
2022-03-14 12:47:00 +0900

    모르겠다

--- Algorithm/baekjoon/백준_1157_단어_공부.py
+++ Algorithm/baekjoon/백준_1157_단어_공부.py
@@ -1,7 +1,28 @@
 import sys
 
+input = sys.stdin.readline
 S = input()
-copy_S = S
+S = S.upper()
+list_count = list()
+max = 0
 
 for i in range(0, len(S)) :
-    print()
+    count = 0
+    for j in range(i, len(S)) :
+        if S[i] == S[j] :
+            count = count + 1
+    list_count.append(count)
+    
+for i in range(0, len(list_count)) :
+    print('list_count', list_count[i])
+    print('befor max', max)
+    if max == list_count[i] :
+        max = -1
+    elif max < list_count[i] :
+        max = list_count[i]
+    print('after max', max)
+        
+if max == -1 :
+    print('?')
+else :
+    print(S[max])
