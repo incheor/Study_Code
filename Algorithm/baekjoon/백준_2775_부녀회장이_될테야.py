356dc209e52faf37a0e7abee4579ae546ffcdd6e
incheor <xornjscjf08@gmail.com>
2022-04-01 17:26:54 +0900

    오늘 공부

--- Algorithm/baekjoon/백준_2775_부녀회장이_될테야.py
+++ Algorithm/baekjoon/백준_2775_부녀회장이_될테야.py
@@ -2,10 +2,14 @@
 T = int(input())
 
 for t in range(T) :
-    # 층 번호
+    # 층 번호 입력
     k = int(input())
-    # 호 번호
+    # 호 번호 입력
     n = int(input())
     
-    for i in range(k + 1) :
-        print(i)
+    # 0층 각 호실 거주민(기준) : 1 ~ n
+    list_std = list(range(1, n + 1))
+    
+    while True :
+        print(list_std)
+        break
